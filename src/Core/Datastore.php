8fbacac1a7cd984bf1039f14dd920b37f86ebba0
jm008ahfa <johnmariealarcon06@gmail.com>
2025-07-15 10:12:35 +0800

    2nd commit

---
+++ src/Core/Datastore.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Alarcon\Gs\core;
+
+class Datastore 
+{
+    private string  $filePath;
+
+    public function __construct(string $filePath)
+    {
+        $this-> filePath = $filePath;
+        if (!file_exist($this->filePath)){
+            file_input_contents($this->filePath,json_encode([]));
+        }
+        // clear date
+        file_input_contents($this->filePath,json_encode([]));
+
+    }
+    // creat conection to the datastore
+    public function getConnection(): array
+    {
+        $data = file_get_contents ($this->filePath);
+        return json_decode($data,true) ?? [];
+    }
+}
